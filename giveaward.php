674cfc4d53f6849601bfb133c69b5c82cb7580f5
MackenzieH <holznecm@oregonstate.edu>
2016-11-01 21:48:44 -0400

    Award-first attempt at stuff

--- giveaward.php
+++ giveaward.php
@@ -3,3 +3,6 @@
 require_once __DIR__ . '/setup/setup.php';
 
 $award = getPageBuilderClass('GiveAward');
+
+$award->renderTemplate('giveaward.html',array('users'=>$users, 'messages' => $messages));
+//I'm not sure what the array is for yet. I'll keep figuring it out - that's why it's probably not right.
