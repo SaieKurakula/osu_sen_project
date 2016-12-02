ef922a921c973cbc7d5d95cb94f7d3be35eaed65
Saie Kurakula <kurakuls@oregonstate.edu>
2016-12-02 10:20:54 -0800

    Updating access to pages based on access level

--- setup/setup.php
+++ setup/setup.php
@@ -22,21 +22,75 @@ if (!(MysqlConn::getConnection('Default'))) {
 
 }
 
-// if (!isset($_SESSION)) {
-// }
-
-
+// Check if page can be accessed without being logged in.
 function isAccessiblePage() {
 
    $unauthenticatedPages = array (
       'login.php',
-      'activation.php',
-      'adduser.php' // THIS IS TEMP WE NEED TO REMOVE THIS
+      'activation.php'
    );
+
+   return checkPages($unauthenticatedPages, false);
+}
+
+// Verifies that user has logged in. If not, then it will return to the main page
+// and go through log in procedure
+if (!isset($_SESSION['username']) && !isAccessiblePage()) {
+   header('Location: login.php');
+}
+
+// checks if page is an admin only page
+function isAdminPage() {
+
+   $adminPages = [
+      'adduser.php',
+      'getreport.php',
+      'businessintel.php',
+   ];
+
+   return checkPages($adminPages);
+
+}
+
+// Checks is user is an admin user
+function isAdminUser() {
+   return isset($_SESSION['accessLevel']) && $_SESSION['accessLevel'] == 'Admin';
+}
+
+// checks is page is a normal user only page
+function isNormalPage() {
+
+   $normalPages = [
+      'giveaward.php',
+      'viewawards.php'
+   ];
+
+   return checkPages($normalPages);
+
+}
+
+// checks if user is logged in and a normal user
+function isNormalUser() {
+   return isset($_SESSION['accessLevel']) && $_SESSION['accessLevel'] == 'Normal';
+}
+
+// if user is on a page that they shouldn't be with their accessLevel, then it redirects it to their home page.
+if ( (isAdminUser() && !isAdminPage()) || (isNormalUser() && !isNormalPage()) ) {
+   header('Location: index.php');
+}
+
+// Verifies the page that the user is on
+function checkPages($pages, $checkCommonPages=true) {
+
+   if ($checkCommonPages) {
+      $pages = array_merge($pages, ['managemyaccount.php', 'logout.php', 'index.php']);
+   }
+
+   $pages[] = 'password.php';
 
    $currentPage = basename($_SERVER['REQUEST_URI']);
 
-   foreach($unauthenticatedPages as $page) {
+   foreach($pages as $page) {
       if (strpos($currentPage, $page) !== false) {
          return true;
       }
@@ -46,23 +100,19 @@ function isAccessiblePage() {
 
 }
 
-// Verifies that user has logged in. If not, then it will return to the main page
-// and go through log in procedure
-if (!isset($_SESSION['username']) && !isAccessiblePage()) {
-   header('Location: login.php');
-}
-
-
+// Wrapper to grab info from form submissions
 function request($requestVar) {
    $returnValue = isset($_REQUEST[$requestVar]) ? $_REQUEST[$requestVar] : null;
    return $returnValue;
 }
 
+// Grab the proper page builder classes
 function getPageBuilderClass($pathInPageBuilders, $pageBuilderClass) {
    require_once(PROJECT_PATH.'/PageBuilders/'. $pathInPageBuilders . $pageBuilderClass . '.php');
    return new $pageBuilderClass();
 }
 
+// Grab the proper helper classes
 function getHelperClass($pathInHelpers, $helperClass) {
    require_once(PROJECT_PATH.'/Helpers/'. $pathInHelpers . $helperClass . '.php');
    return new $helperClass();
