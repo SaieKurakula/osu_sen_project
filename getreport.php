f2c07ac6b606437e263f51992233f26eaa080a32
Darin Etherington <etherind@flip1.engr.oregonstate.edu>
2016-12-02 19:44:43 -0800

    getreport updates

--- getreport.php
+++ getreport.php
@@ -4,17 +4,24 @@ require_once __DIR__ . '/setup/setup.php';
 
 $getreport = getPageBuilderClass('','GetReport');
 
+$jsonTable1 = $getreport->getGraph1();
+$jsonTable2 = $getreport->getGraph2();
+$jsonTable3 = $getreport->getGraph3();
+$jsonTable4 = $getreport->getGraph4();
 //$messages = [];
 
 	//Will dynamically generate this from DB:
 //$awardsPerRegion = $getreport->getAwardsPerRegion();
 
-$reportType = request('reporttype');
+// $reportType = request('reporttype');
 // This is when the "get report type" has been submitted
-if ($reportType == 'Awards by Region') {
+// if ($reportType == 'Awards by Region') {
 
-}
+// }
 
 $getreport->renderTemplate('getreport.html',
-	array()
+	array('jsonTable1'=>$jsonTable1,
+			'jsonTable2'=>$jsonTable2,
+			'jsonTable3'=>$jsonTable3,
+			'jsonTable4'=>$jsonTable4)
 );
